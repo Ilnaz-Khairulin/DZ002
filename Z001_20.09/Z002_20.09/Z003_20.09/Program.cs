41078a73ce407fee4d85a68588767dac3ccf2447
Ilnaz <72466445@mail.ru>
2022-09-20 22:44:57 +0500

    Z003_1

--- Z001_20.09/Z002_20.09/Z003_20.09/Program.cs
+++ Z001_20.09/Z002_20.09/Z003_20.09/Program.cs
@@ -5,21 +5,20 @@ internal partial class Program
 {
     private static void Main(string[] args)
     {
-        int[] n = {1,57,3,89,75,6,7};
+        int[] n = {9,8,7,6,5,4,3};
 
-        int count = 0;
-System.Console.WriteLine(n[4]);
-        while (n[0] <= n.Length)
+        int g = 0;
+
+        while(n[0] <= n.Length)
         {
-
-            count =count+ n[0];
-            System.Console.WriteLine(count);
+            g = g + n[0];
+            System.Console.WriteLine(g);
             n[0]++;
         }
         
-    Console.WriteLine(count);
-        count = count/7;
+        Console.WriteLine(g);
+        g = g/7;
 
-        Console.WriteLine(count);
+        Console.WriteLine(g);
     }
 }
