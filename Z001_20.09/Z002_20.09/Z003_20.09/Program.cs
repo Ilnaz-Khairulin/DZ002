bd853f95c019b7a001b0ac7b9107d3f93677f0f0
Ilnaz <72466445@mail.ru>
2022-09-20 22:28:49 +0500

    Z003

---
+++ Z001_20.09/Z002_20.09/Z003_20.09/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] n = {1,57,3,89,75,6,7};
+
+        int count = 0;
+System.Console.WriteLine(n[4]);
+        while (n[0] <= n.Length)
+        {
+
+            count =count+ n[0];
+            System.Console.WriteLine(count);
+            n[0]++;
+        }
+        
+    Console.WriteLine(count);
+        count = count/7;
+
+        Console.WriteLine(count);
+    }
+}
