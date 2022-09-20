ba3f748884fc8d7ad2405f7ff1788c1b0f5d26cc
Ilnaz <72466445@mail.ru>
2022-09-20 16:16:11 +0500

    не получилось еще

--- Z001_20.09/Program.cs
+++ Z001_20.09/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 456;
+        int[] aray = new int[3];
+        if (n>=0)
+        {
+            aray[0] = n % 10;
+            Console.WriteLine(aray[0]);
+            n = n / 10;
+            Console.WriteLine(n);
+            aray[0]++;
+        }
+         else
+        {
+      Console.WriteLine(aray[1]);  
+    }
+    
+    }
+}
